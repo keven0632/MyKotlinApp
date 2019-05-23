cc13df6bfb390294638029af7908999d1669beaa
keven <keven0632@gmail.com>
2019-05-23 19:01:20 +0800

    second

---
+++ app/src/main/java/com/keven/mykotlinapp/Person.kt
@@ -0,0 +1,10 @@
+package com.keven.mykotlinapp
+
+/**
+ * Created by zhengjian on 2019/5/23.
+ */
+class Person {
+    var name: String = "keven"
+    var age: Int = 18
+    var address:String="北京"
+}
